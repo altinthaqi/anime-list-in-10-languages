855a8732c60ad4f73f1d5a2fb945b0609e880749
Altin Thaci <altin.thaci@fresha.com>
2024-05-01 01:35:08 +0200

    rails: Create and Read animes

--- epsi-rail/app/controllers/animes_controller.rb
+++ epsi-rail/app/controllers/animes_controller.rb
@@ -1,5 +1,25 @@
 class AnimesController < ApplicationController
   def index
-    @animes = Anime.all
+    @animes = Anime.all.order(:created_at).reverse_order
   end
+
+  def show
+    @anime = Anime.find(params[:id])
+  end
+
+  def create
+    @anime = Anime.new(anime_params)
+
+    if @anime.save
+      redirect_to animes_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+  
+  private
+  def anime_params
+    params.require(:anime).permit(:title)
+  end
+
 end
